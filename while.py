d879a58fc60150d06792e9ffd2b535f31c8b1c70
wandy <wandywahidin53@gmail.com>
2021-10-20 03:02:28 +0700

    My First python code for dan while

--- while.py
+++ while.py
@@ -1,5 +1,6 @@
 # Perulangan dengan While adalah perulangan yang tidak diketahui akan berapa kali dilakukan perulangan nya
 
+"""
 jumlah_buku = 10
 print(f'jumlah buku yang ada adalah {jumlah_buku} buku')
 jumlah_buku_yang_dibaca = 0
@@ -10,3 +11,17 @@ while jumlah_buku_yang_dibaca < jumlah_buku:
     print(f'buku ke {jumlah_buku_yang_dibaca} sudah dibaca')
 
 print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_dibaca}')
+"""
+
+jumlah_buku = 10
+print(f'jumlah buku yang ada adalah {jumlah_buku} buku')
+total_baca = 0
+jumlah_buku_yang_dibaca_dan_dipahami = 0
+
+while total_baca < jumlah_buku * 2:
+    total_baca = total_baca + 1
+    if jumlah_buku_yang_dibaca_dan_dipahami == jumlah_buku - 1:
+        print(f'buku ke {jumlah_buku_yang_dibaca_dan_dipahami + 1} belum paham')
+    else:
+        jumlah_buku_yang_dibaca_dan_dipahami = jumlah_buku_yang_dibaca_dan_dipahami + 1
+        print(f'buku ke {jumlah_buku_yang_dibaca_dan_dipahami} sudah dibaca dan dipahami')
